5f07d88061dd409724e877a5e625ec4ae13b7af0
Seth Trowbridge <seth111@gmail.com>
2022-04-16 00:05:12 -0400

    nav/history elements

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
 // @ts-check
-import { useContext } from "react";
 import { html } from "html";
-import * as Store from "./store.js";
+import { Provide, Consume } from "./store.js";
 
 const Deep = () =>
 {
-    const [State, Dispatch] = Store.Consume();
-    return html`<p>${State.Current.Key}</p>`;
+    const [State, Dispatch] = Consume();
+    return html`
+    <div>
+        <p>Previous: ${State.Previous.Key} | ${State.Previous.Ind}</p>
+        <p>Current:  ${State.Current.Key} | ${State.Current.Ind}</p>
+    </div>`;
+};
+
+const Log = () => 
+{
+    const [State, Dispatch] = Consume();
+
+    /** @type {(i:number)=>({background:string})} */
+    const colorize = inIndex => ({background: inIndex==State.Current.Ind ? "red" : "none"});
+    return html`
+    <ol>
+        ${State.History.map((h, i)=>html`
+        <li style=${ colorize(i) } onClick=${ ()=>Dispatch({Type:"pick", Payload:i}) }>
+            ${ h }
+        </li>`)}
+    </ol>`;
+};
+
+const Add = () =>
+{
+    const [State, Dispatch] = Consume();
+    /** @type {Store.Actions} */
+    const action = {Type:"push", Payload:`Page-${Math.floor(Math.random()*1000)}`};
+    return html`
+    <button onClick=${()=>Dispatch(action)}>
+        add random
+    </button>`;
 };
 
 export default html`
-    <${Store.Provide}>
+    <${Provide}>
         <div>
             <h1>History!</h1>
+            <${Add}//>
             <${Deep}//>
+            <${Log}//>
         </div>
     <//>
 `;
